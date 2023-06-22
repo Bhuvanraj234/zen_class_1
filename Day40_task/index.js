1e19d1bb4575b5309597d68168be0d89c1574789
Bhuvanraj Somasundaram <118756855+Bhuvanraj234@users.noreply.github.com>
2023-06-22 09:08:41 +0530

    Update index.js

--- Day40_task/index.js
+++ Day40_task/index.js
@@ -1,15 +1,13 @@
-
 import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
 let rooms = [];
 let bookings = [];
 
-
 app.post('/rooms', (req, res) => {
-  console.log(req.body);
   const { numberOfSeats, amenities, pricePerHour } = req.body;
   const roomId = generateRoomId();
   const room = { roomId, numberOfSeats, amenities, pricePerHour };
@@ -17,11 +15,9 @@ app.post('/rooms', (req, res) => {
   res.status(201).json({ message: 'Room created successfully.', room });
 });
 
-
 app.post('/bookings', (req, res) => {
   const { customerName, date, startTime, endTime, roomId } = req.body;
-  
- 
+
   if (isRoomBooked(roomId, date, startTime, endTime)) {
     return res.status(409).json({ message: 'Room already booked for the given date and time.' });
   }
@@ -32,15 +28,13 @@ app.post('/bookings', (req, res) => {
   res.status(201).json({ message: 'Room booked successfully.', booking });
 });
 
-
 app.get('/rooms/bookings', (req, res) => {
   const roomsWithBookings = rooms.map(room => {
     const bookedData = bookings.filter(booking => booking.roomId === room.roomId);
-    return { ...room, bookedData };
+    return { ...room, bookings: bookedData };
   });
   res.status(200).json(roomsWithBookings);
 });
-
 
 app.get('/customers/bookings', (req, res) => {
   const customersWithBookings = bookings.map(booking => {
@@ -50,26 +44,20 @@ app.get('/customers/bookings', (req, res) => {
   res.status(200).json(customersWithBookings);
 });
 
-
 app.get('/customers/:customerName/bookings', (req, res) => {
   const { customerName } = req.params;
   const customerBookings = bookings.filter(booking => booking.customerName === customerName);
   res.status(200).json(customerBookings);
 });
 
-
 function generateRoomId() {
- 
   return Math.floor(100000 + Math.random() * 900000);
 }
 
-// Helper function to generate a booking ID
 function generateBookingId() {
-  // Generate a random 6-digit number as the booking ID
   return Math.floor(100000 + Math.random() * 900000);
 }
 
-// Helper function to check if a room is already booked for the given date and time
 function isRoomBooked(roomId, date, startTime, endTime) {
   return bookings.some(
     booking =>
