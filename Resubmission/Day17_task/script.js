82888469b77aee984c65b6ca72dc801560649ef6
Bhuvanraj Somasundaram <118756855+Bhuvanraj234@users.noreply.github.com>
2023-07-28 12:55:49 +0530

    Add files via upload

---
+++ Resubmission/Day17_task/script.js
@@ -0,0 +1,38 @@
+ window.addEventListener("DOMContentLoaded", function () {
+            const h1Element = document.querySelector("h1");
+
+            setTimeout(() => {
+                h1Element.innerHTML = 10;
+            setTimeout(() => {
+                h1Element.innerHTML = 9;
+            setTimeout(() => {
+                h1Element.innerHTML = 8;
+            setTimeout(() => {
+                h1Element.innerHTML = 7;
+            setTimeout(() => {
+                h1Element.innerHTML = 6;
+            setTimeout(() => {
+                h1Element.innerHTML = 5;
+            setTimeout(() => {
+                h1Element.innerHTML = 4;
+            setTimeout(() => {
+                h1Element.innerHTML = 3;
+            setTimeout(() => {
+                h1Element.innerHTML = 2;
+            setTimeout(() => {
+                h1Element.innerHTML = 1;
+            setTimeout(() => {
+                h1Element.innerHTML = "Happy Independence Day";
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+            }, 1000);
+        });
+   
